88713a2c6f2cf3fe38aece7385698b9894923ebd
zhujz-git <249382041@qq.com>
2022-08-28 20:37:27 +0800

    20220828

--- movie_info_get/get_douban_movie.py
+++ movie_info_get/get_douban_movie.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-import reportlab_setpdf
+import re
 
 
 def get_html_content(url):
@@ -53,6 +53,105 @@ def bs4_parse_html(mlist, htxt):
         print('爬取失败', e)
 
 
+def bs4_parse_movie_info_html(htxt):
+    soup = BeautifulSoup(htxt, 'html.parser')
+    cont = soup.find('div', id='content')
+    minfo = {}
+    minfo['top_no'] = cont.find('div', 'top250').span.string
+    minfo['reviewed'] = cont.h1.span.string
+    minfo['year'] = cont.h1.find('span', 'year').string
+    minfo['allpic_herf'] = cont.find('div', id='mainpic').a['href']
+    minfo['mainpic'] = cont.find('div', id='mainpic').img['src']
+    tinfo = cont.find('div', id='info')
+    minfo['info'] = get_movie_info(tinfo)
+    related_info = cont.find('div', 'related-info')
+    minfo['related-info'] = ''.join(related_info.h2.stripped_strings) + '\n' + '\n'.join(
+        related_info.find('span', property="v:summary").string)
+    minfo['celebrities'] = get_movie_celebrities(
+        cont.find('div', id='celebrities'))
+    minfo['related-pic'] = get_related_pic(cont.find('div', 'related-pic'))
+
+
+def get_movie_info(tinfo):
+    # 找出影片内容
+    info = []
+    span = tinfo.span
+    iOneItem = True
+    item_str = ''
+    while span:
+        if span.name == 'br':
+            iOneItem = False
+            info.append(item_str)
+            item_str = ''
+
+        if iOneItem:
+            item_str += ''.join(span.stripped_strings)
+
+        span = span.next_sibling
+        iOneItem = True
+    return info
+
+# 获取演职员列表
+
+
+def get_movie_celebrities(celebrities):
+    cd = {}
+    cd['title'] = celebrities.i.string
+    li = []
+    pat = re.compile(r'(?<=\()(.*)(?=\))')
+    for cele in celebrities.find_all('li', 'celebrity'):
+        title = cele.a['title']
+        role = cele.find('span', 'role').string
+        # 匹配（……）里的url
+        url = pat.search(cele.find('div', 'avatar')['style']).group(0)
+        li.append((title, role, url))
+    cd['li'] = li
+    return cd
+
+# 获取电影所有图片链接
+
+
+def get_related_pic(related_pic):
+    title = related_pic.i.string
+    pic_num = related_pic.span.find(string=re.compile('图片'))
+    href = related_pic.span.find(href=re.compile('photo'))
+    pic_num = href.string
+    ahref = href['href']
+    ahref = ahref.rstrip('all_photos')
+    type_s = ('S', 'R', 'W')
+    pic_url_list = []
+    for s in type_s:
+        url = ahref+'photos?type='+s
+        pic_url_list.append(get_all_pic(url))
+
+# 获取一个页面的所有图片链接
+def get_all_pic(url):    
+    htxt = get_html_content(url)
+    soup = BeautifulSoup(htxt, 'html.parser')
+    cont = soup.find('div', id='content')
+    mdict = {}
+    mdict['title'] = cont.h1.string
+    # 获取图片数量
+    pat = re.compile(r'(\d+)')
+    pic_num = cont.find('span', 'count').string
+    npic = int(pat.search(pic_num).group(0))
+
+    #遍历所有页面里的图片，获取地址 30张 一页
+    pic_id_list = []
+    for i in range(npic/30+1):
+        nurl = url+'&start=' + '{}'.format(i*30)
+        soup = BeautifulSoup(get_html_content(nurl), 'html.parser')
+        cont = soup.find('div', id='content')
+        for li in cont.select('li[data-id]'):
+            pic_id = li['data-id']
+            pic_href = li.a['href']
+            soup = BeautifulSoup(get_html_content(pic_href), 'html.parser')
+            cont = soup.find('div', id='content')
+            img_src = cont.img['src']
+            pic_id_list.append((pic_id, img_src))
+    mdict['list'] = pic_id_list
+
+
 def get_movie_list():
     url = 'https://movie.douban.com/top250?start='
     n = 25
@@ -65,8 +164,9 @@ def get_movie_list():
         print('success dump file :movie_data.json')
     return mlist
 
-def get_movie_info(mdict):
-    
-    pass
 
-get_movie_list()
+def get_movie_list_info(mlist):
+    minfo_list = []
+    for m in mlist:
+        html = get_html_content(m['href'])
+        minfo_list.append(bs4_parse_movie_info_html(html))
